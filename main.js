cf9d6b7e9543871d267d3de7c8a95cfa0bf4c567
AliciaManyeka <amanyeka8@gmail.com>
2024-05-09 08:08:56 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const menu = document.querySelector('.hamburger')
+const menuOne = document.querySelector('.pc-nav')
+
+menu.addEventListener('click', function(){
+    menuOne.classList.toggle('alicia')
+    menu.classList.toggle('active-lines')
+})
+const inputEl = document.querySelector(".input");
+
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+updateBody();
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "black";
+    bodyEl.style.color="white";
+  } else {
+    bodyEl.style.background = "white";
+    bodyEl.style.color="black";
+  }
+}
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
