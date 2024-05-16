128c332feb43434c508398019395b80e556df3a3
AliciaManyeka <amanyeka8@gmail.com>
2024-05-16 12:00:13 +0200

    gg

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const menu = document.querySelector('.hamburger')
-const menuOne = document.querySelector('.pc-nav')
+const menuOne = document.querySelector('.navbar')
 
 menu.addEventListener('click', function(){
     menuOne.classList.toggle('alicia')
@@ -8,6 +8,30 @@ menu.addEventListener('click', function(){
 const inputEl = document.querySelector(".input");
 
 const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+updateBody();
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "black";
+    bodyEl.style.color="white";
+  } else {
+    bodyEl.style.background = "white";
+    bodyEl.style.color="black";
+  }
+}
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
+
 
 inputEl.checked = JSON.parse(localStorage.getItem("mode"));
 
